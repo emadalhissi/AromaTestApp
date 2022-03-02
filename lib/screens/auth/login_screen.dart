8014bc0f702cb64fadfce3cea3ace2e2f1be0898
emadalhissi <emadalhissi@gmail.com>
2022-03-02 10:51:14 +0300

    test

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -12,6 +12,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      // body: FloatingActionButton()
     );
   }
 }
